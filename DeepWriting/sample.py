910ebb5260ffe5dde581df1c4b84b1c3dfdf95d2
Nicole Fronda <nicole@Centauri.local>
2017-03-19 16:52:53 -0700

    completed running training on ghosttalkers reviews

--- DeepWriting/sample.py
+++ DeepWriting/sample.py
@@ -12,12 +12,14 @@ from model import Model
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--save_dir', type=str, default='save',
+    parser.add_argument('--save_dir', type=str, default='save/save_ghosttalkers',
                        help='model directory to store checkpointed models')
-    parser.add_argument('-n', type=int, default=1200, 
+    parser.add_argument('-n', type=int, default=200,
                        help='number of words to sample')
     parser.add_argument('--prime', type=str, default=' ',
                        help='prime text')
+    parser.add_argument('--pick', type=int, default=1,
+                       help='1 = weighted pick, 2 = beam search pick')
     parser.add_argument('--sample', type=int, default=1,
                        help='0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces')
 
@@ -31,12 +33,12 @@ def sample(args):
         words, vocab = cPickle.load(f)
     model = Model(saved_args, True)
     with tf.Session() as sess:
-        tf.initialize_all_variables().run()
-        saver = tf.train.Saver(tf.all_variables())
+        tf.global_variables_initializer().run()
+        saver = tf.train.Saver(tf.global_variables())
         ckpt = tf.train.get_checkpoint_state(args.save_dir)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(sess, ckpt.model_checkpoint_path)
-            print(model.sample(sess, words, vocab, args.n, args.prime, args.sample))
+            print(model.sample(sess, words, vocab, args.n, args.prime, args.sample, args.pick))
 
 if __name__ == '__main__':
     main()
